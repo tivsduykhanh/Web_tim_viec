d92abc8fc58a6847ee0bc9fbabe9fc3d056866b4
duykhanh <ddkhanh280200@gmail.com>
2021-11-19 19:56:29 +0700

    layout home

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,12 @@
 use App\Http\Controllers\Admin\Maincontroller;
 use Illuminate\Support\Facades\Route;
 use \App\Http\Controllers\Admin\Users\LoginController;
+use App\Http\Controllers\HomeController;
 
 Route::get('admin/users/login', [LoginController::class, 'index'])->name('login');
 Route::post('admin/users/login/timviec', [LoginController::class, 'timviec']);
+
+Route::get('/', [HomeController::class, 'index']);
 
 Route::middleware(['auth'])->group(function () {
     Route::get('admin', [Maincontroller::class, 'index'])->name('admin');
