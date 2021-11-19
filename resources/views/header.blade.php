cc29474de33b9bff08700fbec5ec53964d8c83a9
quocbao144 <quocbao14412@gmail.com>
2021-11-19 22:00:54 +0700

    Tạo form CV

---
+++ resources/views/header.blade.php
@@ -0,0 +1,27 @@
+  <!-- Mobile Specific Meta -->
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <!-- Favicon-->
+  <link rel="shortcut icon" href="/template/user/img/fav.png">
+  <!-- Author Meta -->
+  <meta name="author" content="codepixer">
+  <!-- Meta Description -->
+  <meta name="description" content="">
+  <!-- Meta Keyword -->
+  <meta name="keywords" content="">
+  <!-- meta character set -->
+  <meta charset="UTF-8">
+  <!-- Site Title -->
+  <title>{{$title}}</title>
+
+  <link href="https://fonts.googleapis.com/css?family=Poppins:100,200,400,300,500,600,700" rel="stylesheet">
+  <!--
+   CSS
+   ============================================= -->
+  <link rel="stylesheet" href="/template/user/css/linearicons.css">
+  <link rel="stylesheet" href="/template/user/css/font-awesome.min.css">
+  <link rel="stylesheet" href="/template/user/css/bootstrap.css">
+  <link rel="stylesheet" href="/template/user/css/magnific-popup.css">
+  <link rel="stylesheet" href="/template/user/css/nice-select.css">
+  <link rel="stylesheet" href="/template/user/css/animate.min.css">
+  <link rel="stylesheet" href="/template/user/css/owl.carousel.css">
+  <link rel="stylesheet" href="/template/user/css/main.css">
--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,13 @@
 use App\Http\Controllers\Admin\Maincontroller;
 use Illuminate\Support\Facades\Route;
 use \App\Http\Controllers\Admin\Users\LoginController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\User\TaocvController;
 
 Route::get('admin/users/login', [LoginController::class, 'index'])->name('login');
 Route::post('admin/users/login/timviec', [LoginController::class, 'timviec']);
+Route::get('user/taocv',[TaocvController::class,'index']);
+Route::get('/', [HomeController::class, 'index']);
 
 Route::middleware(['auth'])->group(function () {
     Route::get('admin', [Maincontroller::class, 'index'])->name('admin');
